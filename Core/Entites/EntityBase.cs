ba28e27fc721add557147ea874d40d099cc3d6dc
taky1 <48655485+taky1@users.noreply.github.com>
2020-04-21 22:54:53 +0100

    Ajoutez des fichiers projet.

---
+++ Core/Entites/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Core.Entites
+{
+  public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+
+}
---
+++ Core/Entites/ProfolioItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entites
+{
+   public class ProfolioItem :EntityBase
+    {
+
+        public string ProjectName { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+
+}
